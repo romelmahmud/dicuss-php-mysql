a1a0265b109bf9cdfc2411d058a649b06a412c03
unknown <romelmahmud.dev@gmail.com>
2024-09-29 03:34:15 +0600

    logo add and navbar create

---
+++ client/header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+
+    <a class="navbar-brand" href="#">
+    <img class ="" src="./public/images/discuss-logo.png" alt="Discuss band logo" width="100" >
+    </a>
+
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-primary-emphasis"  href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-primary-emphasis" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-primary-emphasis" href="#">Category</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-primary-emphasis" href="#">Latest Question</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
